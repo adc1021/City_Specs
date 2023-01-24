bf0ca922d647b8533743cc846a9ba98bbb9fa8cd
Anthony_Chiodi <adc8wlc@gmail.com>
2023-01-24 15:06:42 -0500

    list of city data to display

--- src/index.js
+++ src/index.js
@@ -23,12 +23,43 @@ const cityScores = async (city) => {
           }
         })
   .then((data) => {
-    debugger
+    console.log(data)
+    const categories = data.categories
+    console.log(categories)
+    return categories
   })
+  .catch((errorResponse) => console.error(errorResponse))
 }
 
-console.log(cityScores('portland-me'))
+// cityScores('houston')
 
-console.log(cityScores('jacksonville'))
+// cityScores('jacksonville')
 
-// console.log(cityScores('chuckville'))
+// cityScores('new-york')
+
+// cityScores('los-angeles')
+
+// cityScores('richmond')
+
+// cityScores('charleston')
+// cityScores('atlanta')
+// cityScores('new-orleans')
+// cityScores('miami')
+// cityScores('boston')
+// cityScores('phoenix')
+// cityScores('albuquerque')
+// cityScores('seattle')
+// cityScores('portland-or')
+// cityScores('boise')
+// cityScores('chicago')
+// cityScores('memphis')
+// cityScores('louisville')
+// cityScores('san-diego')
+// cityScores('des-moines')
+// cityScores('omaha')
+// cityScores('st-louis')
+// cityScores('detroit')
+// cityScores('denver')
+// cityScores('denver')
+// cityScores('columbus')
+
