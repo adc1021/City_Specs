974f1ab643b0a84841981047e438410724ded8b7
Anthony_Chiodi <adc8wlc@gmail.com>
2023-01-25 17:33:19 -0500

    adding dom content wrapper to load DOM

--- src/index.js
+++ src/index.js
@@ -3,56 +3,57 @@ import { PolarAreaController, ArcElement } from 'chart.js/auto';
 import { getRelativePosition } from 'chart.js/helpers';
 import { cityScores } from './scripts/datafetch';
 
-// const chart = new chartTools.Chart(ctx, {
-//   type: "polarArea",
-
-// })
-
-(async function() {
-  const res = await cityScores('houston');
-  const housing = Math.floor(res.categories[0].score_out_of_10)
-  const costOfLiving = Math.floor(res.categories[1].score_out_of_10)
-  const commute = Math.floor(res.categories[5].score_out_of_10)
-  const safety = Math.floor(res.categories[7].score_out_of_10)
-  const healthcare = Math.floor(res.categories[8].score_out_of_10)
-  const education = Math.floor(res.categories[9].score_out_of_10)
-  new chartTools.Chart(
-    document.getElementById('acquisitions'),
-    {
-      type: 'polarArea',
-      data: {
-        labels: [
-          'Housing',
-          'Cost of Living',
-          'Commute',
-          'Safety',
-          'Healthcare',
-          'Education'
-        ],
-        datasets: [{
-          label: 'Houston',
-          data: [housing, costOfLiving, commute, safety, healthcare, education],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(75, 192, 192)',
-            'rgb(255, 205, 86)',
-            'rgb(201, 203, 207)',
-            'rgb(54, 162, 235)',
-            ' #FFA500'
-          ]
-        }]
-      }
-    }
-  );
-})();
 
 
-let city = document.getElementById('city-selector')
-console.log(city)
-// async function main() {
-//   use dom.innerText
-//   const data = await cityScores('houston');
 
-// }
 
-// main()
+  document.addEventListener('DOMContentLoaded', () => {
+    (async function() {
+      let selectCity = document.getElementById('city-selector') //
+      let value = selectCity.value
+      selectCity.addEventListener('change', ((e) => {
+        cityScores(e.target.value)
+        value = e.target.value
+      }))
+
+      let res = await cityScores(value); // the await is preventing dom manipulation
+      // How can I make my chart change on change?
+      const housing = Math.floor(res.categories[0].score_out_of_10)
+      const costOfLiving = Math.floor(res.categories[1].score_out_of_10)
+      const commute = Math.floor(res.categories[5].score_out_of_10)
+      const safety = Math.floor(res.categories[7].score_out_of_10)
+      const healthcare = Math.floor(res.categories[8].score_out_of_10)
+      const education = Math.floor(res.categories[9].score_out_of_10)
+      console.log(document.getElementById('city-selector'))
+
+      new chartTools.Chart(
+        document.getElementById('acquisitions'),
+        {
+          type: 'polarArea',
+          data: {
+            labels: [
+              'Housing',
+              'Cost of Living',
+              'Commute',
+              'Safety',
+              'Healthcare',
+              'Education'
+            ],
+            datasets: [{
+              label: 'Houston',
+              data: [housing, costOfLiving, commute, safety, healthcare, education],
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(75, 192, 192)',
+                'rgb(255, 205, 86)',
+                'rgb(201, 203, 207)',
+                'rgb(54, 162, 235)',
+                ' #FFA500'
+              ]
+            }]
+          }
+        }
+        );
+      })();
+
+  })
